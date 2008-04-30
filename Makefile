turbana/dor

commit b5ab7a454fdceeb544471100d7773a89b30652de

In Makefile: kernel.bin now depends on kernel.ld

Makefile
@@ -76,6 +76,8 @@ $(BDIR) :
 	@echo "DIR  $@"
 	@mkdir $@
 
+kernel.bin : $(SDIR)/kernel.ld
+
 kernel.bin : $(addprefix $(BDIR)/, $(OBJS))
 	@echo "LINK $@"
 	@$(LD) $(LDFLAGS) -T $(SDIR)/kernel.ld -o kernel.bin $^

commit 3b281f00715b5d90bb476a03d2b5651a5eac01dc

Fixed build regression introduced in b5ab7a454fdceeb544471100d7773a89b30652de.
Apparently having two rules for kernel.bin (one depending on the linker script
and the other on all the .o's) screws things up. I get wierd linker errors
about overlapping segments with the two rules. I should look into this more.

Makefile
@@ -88,8 +88,6 @@ $(BDIR) :
 	@echo "DIR  $@"
 	@mkdir $@
 
-kernel.bin : $(SDIR)/kernel.ld
-
 kernel.bin : $(addprefix $(BDIR)/, $(OBJS))
 	@echo "LINK $@"
 	@$(LD) $(LDFLAGS) -T $(SDIR)/kernel.ld -o kernel.bin $^