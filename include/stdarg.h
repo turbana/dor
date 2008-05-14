e7f0f3a8e3eba7a22a1a5cb571d270ea8fad051d
Ian Clark <turbana@gmail.com>
2008-05-14 16:33:28 -0700

    Added va_args functionality through a GCC extension.

---
+++ include/stdarg.h
@@ -0,0 +1,10 @@
+#ifndef __STDARG_H
+#define __STDARG_H
+
+/* use GCC extensions for varargs */
+typedef __builtin_va_list va_list;
+#define va_start	__builtin_va_start
+#define va_end		__builtin_va_end
+#define va_arg		__builtin_va_arg
+
+#endif
